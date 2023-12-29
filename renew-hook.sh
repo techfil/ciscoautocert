efaa73b38de884f646ed8053097d4c9febf5190a
techfil <91088474+techfil@users.noreply.github.com>
2023-12-29 16:03:53 +0100

    Update renew-hook.sh to show timestamps and clear SUCCESS / FAILURE

--- renew-hook.sh
+++ renew-hook.sh
@@ -7,9 +7,9 @@ if [ -f privkey.pem ] && [ -f fullchain.pem ]; then
    openssl pkcs12 -export -in combined.pem -name CISCOAUTOCERT -passout pass:cisco -out /root/ciscoautocert.p12
    mv ciscoautocert.p12 /bootflash/guest-share/
    rm /root/combined.pem
-   echo "Certificate in $RENEWED_LINEAGE was renewed, and is ready to be imported into IOS-XE at $(date)." >> /home/guestshell/ciscoautocert.log
+   echo "$(date) - SUCCESS - Certificate in $RENEWED_LINEAGE was renewed, and is ready to be imported into IOS-XE." >> /home/guestshell/ciscoautocert.log
    exit 0
 else
-   echo "Failure: I did not find the certificate files, something went wrong with Certbot. Aborting." >> /home/guestshell/ciscoautocert.log
+   echo "$(date) - FAILURE - Could not find the certificate files, something went wrong. Aborting." >> /home/guestshell/ciscoautocert.log
    exit 1
 fi
